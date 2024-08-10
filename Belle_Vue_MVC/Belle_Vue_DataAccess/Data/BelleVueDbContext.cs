875557f01b4f5699b39682359402bf48b277269c
Hugo <1946091@cegepmontpetit.ca>
2024-08-10 00:19:16 -0400

    PROGRESS : Start project

---
+++ Belle_Vue_MVC/Belle_Vue_DataAccess/Data/BelleVueDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Belle_Vue_Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Belle_Vue_DataAccess.Data
+{
+	public class BelleVueDbContext : IdentityDbContext
+	{
+
+		public BelleVueDbContext(DbContextOptions<BelleVueDbContext> options) : base(options) { }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+
+			//// Seed data using the HasData method
+			//ModelBuilderDataGenerator.GenerateData(modelBuilder);
+		}
+	}
+}
---
+++ Belle_Vue_MVC/Belle_Vue_MVC/Program.cs
@@ -0,0 +1,51 @@
+using Belle_Vue_DataAccess.Data;
+using Belle_Vue_Services.IServices;
+using Belle_Vue_Services.Services;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using System.Text.Json.Serialization;
+using Belle_Vue_DataAccess.Initializer;
+using Microsoft.IdentityModel.Tokens;
+using Belle_Vue_Utility.Constants;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<BelleVueDbContext>(options =>
+	options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+	.AddEntityFrameworkStores<BelleVueDbContext>();
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.UseMigrationsEndPoint();
+}
+else
+{
+	app.UseExceptionHandler("/Home/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+	name: "default",
+	pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+app.Run();
